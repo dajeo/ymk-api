c59d90a275ce59c1285ad4125012ce71607bba2e
Danil Chernyshov <headcrabj1@gmail.com>
2023-03-25 15:53:58 +0000

    Fix cors

--- api/serverless.js
+++ api/serverless.js
@@ -5,7 +5,10 @@ const app = Fastify({
     logger: false
 })
 
-app.register(cors)
+app.register(cors, {
+    origin: 'https://ymk.vercel.app',
+    methods: ['POST']
+})
 
 app.register(import('../functions/index.js'), {
     prefix: '/'
